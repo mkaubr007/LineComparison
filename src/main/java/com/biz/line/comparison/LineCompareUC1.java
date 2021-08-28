1c0345b8bdd563cd27a42cc076ca84ea4f637126
mkaubr007 <mkaubr007@gmail.com>
2021-08-28 13:08:11 +0530

    sqrt

--- src/main/java/com/biz/line/comparison/LineCompareUC1.java
+++ src/main/java/com/biz/line/comparison/LineCompareUC1.java
@@ -2,18 +2,20 @@ package com.biz.line.comparison;
 
 import java.util.Scanner;
 
+
 public class LineCompareUC1 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("enter x1 value");
-        int x1=sc.nextInt();
+        Integer x1=sc.nextInt();
         System.out.println("enter x2 value");
-        int x2=sc.nextInt();
+        Integer x2=sc.nextInt();
         System.out.println("enter y1 value");
-        int y1=sc.nextInt();
+        Integer y1=sc.nextInt();
         System.out.println("enter y2 value");
-        int y2=sc.nextInt();
-        int length=(int)Math.sqrt((x2-x1)^2+(y2-y1)^2);
-        System.out.println("length: "+length);
+        Integer y2=sc.nextInt();
+        Integer length=(int)Math.sqrt(x2-x1)^2+(y2-y1)^2;
+        System.out.println(length);
     }
 }
+
