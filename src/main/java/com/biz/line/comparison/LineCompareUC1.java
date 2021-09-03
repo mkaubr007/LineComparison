635ff2867bfce06a565bbaf0871f0bd4e4730bff
mkaubr007 <mkaubr007@gmail.com>
2021-09-03 11:13:51 +0530

    line length added

--- src/main/java/com/biz/line/comparison/LineCompareUC1.java
+++ src/main/java/com/biz/line/comparison/LineCompareUC1.java
@@ -6,13 +6,10 @@ import java.util.Scanner;
 public class LineCompareUC1 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter x1 value");
+        System.out.println("enter the value for line x1,x2,y1,y2");
         Integer x1=sc.nextInt();
-        System.out.println("enter x2 value");
         Integer x2=sc.nextInt();
-        System.out.println("enter y1 value");
         Integer y1=sc.nextInt();
-        System.out.println("enter y2 value");
         Integer y2=sc.nextInt();
         Integer length=(int)Math.sqrt(x2-x1)^2+(y2-y1)^2;
         System.out.println("length :"+length);
