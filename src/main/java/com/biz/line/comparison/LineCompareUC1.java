1d94cab2bfa77c3585c62310e26f27c4dd71d458
mkaubr007 <mkaubr007@gmail.com>
2021-09-03 11:34:53 +0530

    equalMethod added

--- src/main/java/com/biz/line/comparison/LineCompareUC1.java
+++ src/main/java/com/biz/line/comparison/LineCompareUC1.java
@@ -6,16 +6,19 @@ import java.util.Scanner;
 public class LineCompareUC1 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter x1 value");
+        System.out.println("For line first enter x1,x2,y1,y2");
         Integer x1=sc.nextInt();
-        System.out.println("enter x2 value");
         Integer x2=sc.nextInt();
-        System.out.println("enter y1 value");
         Integer y1=sc.nextInt();
-        System.out.println("enter y2 value");
         Integer y2=sc.nextInt();
-        Integer length=(int)Math.sqrt(x2-x1)^2+(y2-y1)^2;
-        System.out.println("length :"+length);
+        System.out.println("For line second enter x3,x4,y3,y4");
+        Integer x3=sc.nextInt();
+        Integer x4=sc.nextInt();
+        Integer y3=sc.nextInt();
+        Integer y4=sc.nextInt();
+        Integer length1=(int)Math.sqrt(x2-x1)^2+(y2-y1)^2;
+        Integer length2=(int)Math.sqrt(x4-x3)^2+(y4-y3)^2;
+        System.out.println(length1.equals(length2));
     }
 }
 
